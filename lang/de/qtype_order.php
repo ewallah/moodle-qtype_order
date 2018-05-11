73dd5b74a6d12b6aa1862081df895ebc0e86b767
eWallah <info@eWallah.net>
2018-05-11 02:35:44 +0000

    first commit

---
+++ lang/de/qtype_order.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The language strings for the order question type.
+ *
+ * @package    qtype_order
+ * @copyright  2007 Adriane Boyd
+ * @author adrianeboyd@gmail.com
+ * @author rdebleu@eWallah.net
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addingorder'] = 'Anordnungsfrage hinzufügen';
+$string['addmoreqblanks'] = 'Weitere Werte hinzufügen';
+$string['defaultresponse'] = 'Ich weiß nicht';
+$string['editingorder'] = 'Anordnungsfrage bearbeiten';
+$string['filloutthreeitems'] = 'Sie müssen mindestens drei Werte in der richtigen Reihenfolge eingeben. Leere Felder werden ignoriert.';
+$string['horizontal'] = 'Werte nebeneinander anzeigen';
+$string['itemno'] = 'Wert {$a}';
+$string['order'] = 'Anordnungsfrage';
+$string['ordersummary'] = 'Werte werden per Drag-and-Drop entweder senkrecht oder nebeneinander eingeordnet.'
